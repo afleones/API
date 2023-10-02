c70ac79b4b5fc10aea67aa72ff3b9f5985d69378
afleones <afleones@misena.edu.co>
2023-10-02 12:02:45 -0500

    enable localhost:5173

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -23,31 +23,6 @@ class CorsMiddleware
         $response->header('Access-Control-Expose-Headers' , 'Authorization');
         $response->header('Access-Control-Max-Age' , '86400');
 
-        // Permitir el acceso desde el origen deseado (cambia 'http://localhost:5173' según tu necesidad)
-        $allowedOrigin = 'http://localhost:5173';
-
-        // Configurar las cabeceras CORS
-        $response->header("Access-Control-Allow-Origin: $allowedOrigin");
-        $response->header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-        $response->header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, weight, otras-cabeceras-que-necesitas");
-
-        // Permitir que las cookies se incluyan en las solicitudes (si es necesario)
-        $response->header("Access-Control-Allow-Credentials: true");
-
-        // Configurar la duración de la caché de las cabeceras preflight (solicitud OPTIONS)
-        if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-            header("Access-Control-Max-Age: 1728000");
-            header("Content-Length: 0");
-            header("Content-Type: text/plain");
-        }
-
-        // Tu lógica de la aplicación va aquí...
-
-        // Ejemplo de respuesta JSON
-        $responseData = array('message' => 'Respuesta desde el servidor PHP');
-        echo json_encode($responseData);
-
-
         return $response;
     }
 }
