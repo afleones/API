ba575f740d4ad0f044f3ae83f96e0fff134a2d88
afleones <afleones@misena.edu.co>
2023-10-03 11:22:50 -0500

    adding womenHealth Endpoint

--- routes/api.php
+++ routes/api.php
@@ -196,7 +196,7 @@ Route::post('login', [AuthContoller::class, 'login']);
     Route::post('saveWomenHealth', [womenhealthController::class, 'store']);
     Route::get('womenHealth', [womenhealthController::class, 'show']);
     Route::post('updateWomenHealth', [womenhealthController::class, 'update']);
-    Route::post('deleteWomenHealth', [womenhealthController::class, 'destroy']);   
+    Route::post('deleteWomenHealth', [womenhealthController::class, 'destroy']);
     /* End */
 
     //Api Routes GenomaX_Documents End
