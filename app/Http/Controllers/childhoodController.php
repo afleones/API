c9d36d3ea45893d6fddd77e0f8d5c4bcf8ea3018
afleones <afleones@misena.edu.co>
2023-10-05 16:32:04 -0500

    ..

--- app/Http/Controllers/childhoodController.php
+++ app/Http/Controllers/childhoodController.php
@@ -2,52 +2,53 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\childhood;
+use App\Models\person;
 use Illuminate\Http\Request;
 
-class childhoodController extends Controller
+class personController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-       $childhood = childhood::selectRaw("id,
-       weight,
-       size,
-       headcircunference,
-       gillperimeter,
-       perimeterwaist,
-       perimeterHip,
-       systolicpressure,
-       diastolicpressure,
-       congenitalAnomaly,                                                           
-       consumptionSubstances,
-       chronicConditions,
-       disability,
-       promotionHealth,
-       completeVaccination,
-       deworming,
-       oralHygiene,
-       optometryPending,
-       problemsDevelopment,
-       signsEda,
-       signsEra,
-       medicineAncestral,
-       nutritionalProblems,
-       malnutrition,
-       overweightobesity,
-       dangerDeath,
-       victimization,
-       unschooling,
-       schoolPerformance,
-       dresserChronic,
-       tripZonesEndemic,
+       //
+       $person = person::selectRaw("id,
+       rol_familiar,
+       primer_nombre,
+       segundo_nombre,
+       primer_apellido,
+       segundo_apellido,
+       tipo_documento,
+       numero_documento,
+       fecha_nacimiento,
+       edad,
+       sexo,
+       identidad_genero,
+       telefono_familiar,
+       nivel_escolaridad,
+       aporta_ingresos,
+       tipo_afiliacion,
+       grupo_atencion_familiar,
+       habla_creole,
+       vacunas_covid,
+       dosis_vacuna,
+       consumo_sustancias,
+       tipo_sustancias,
+       persona_recibe_visita,
+       cursos_vida_integrantes,
+       ocupacion_integrantes,
+       situacion_discapacidad,
+       tipo_discapacidad,
+       atencion_integral,
+       vinculacion_sgsss,
+       percepcion_funcionalidad,
+       cuidador_principal,
+       escala_zarit,
        userid,
-       updated_at,
-       created_at
+       viviendaid
        ")->get();
-       return $childhood;
+       return $person;
     }
 
     /**
@@ -57,71 +58,94 @@ class childhoodController extends Controller
     {
         //
         $data = $request->all();
+
+        
         // Creamos un nuevo objeto del modelo
-        $childhood = new childhood();
-        $childhood->weight = $data['weight'];
-        $childhood->size = $data['size'];
-        $childhood->headcircunference = $data['headcircunference'];
-        $childhood->gillperimeter = $data['gillperimeter'];
-        $childhood->perimeterwaist = $data['perimeterwaist'];
-        $childhood->perimeterHip = $data['perimeterHip'];
-        $childhood->systolicpressure = $data['systolicpressure'];
-        $childhood->diastolicpressure = $data['diastolicpressure'];
-        $childhood->congenitalAnomaly = $data['congenitalAnomaly'];
-        $childhood->consumptionSubstances = $data['consumptionSubstances'];
-        $childhood->chronicConditions = $data['chronicConditions'];
-        $childhood->disability = $data['disability'];
-        $childhood->promotionHealth = $data['promotionHealth'];
-        $childhood->completeVaccination = $data['completeVaccination'];
-        $childhood->deworming = $data['deworming'];
-        $childhood->oralHygiene = $data['oralHygiene'];
-        $childhood->optometryPending = $data['optometryPending'];
-        $childhood->problemsDevelopment = $data['problemsDevelopment'];
-        $childhood->signsEda = $data['signsEda'];
-        $childhood->signsEra = $data['signsEra'];
-        $childhood->nutritionalProblems = $data['nutritionalProblems'];
-        $childhood->malnutrition = $data['malnutrition'];
-        $childhood->overweightobesity = $data['overweightobesity'];
-        $childhood->dangerDeath = $data['dangerDeath'];
-        $childhood->victimization = $data['victimization'];
-        $childhood->unschooling = $data['unschooling'];
-        $childhood->schoolPerformance = $data['schoolPerformance'];
-        $childhood->dresserChronic = $data['dresserChronic'];
-        $childhood->tripZonesEndemic = $data['tripZonesEndemic'];
-        $childhood->userid = $data['userid'];
+        $person = new person();
+
+        $person->rol_familiar = $data['rol_familiar'] ?? '';
+        
+        $person->primer_nombre = $data['primer_nombre'] ?? '';
+        $person->segundo_nombre = $data['segundo_nombre'] ?? '';
+        $person->primer_apellido = $data['primer_apellido'] ?? '';
+        $person->segundo_apellido = $data['segundo_apellido'] ?? '';
+        $person->tipo_documento = $data['tipo_documento'] ?? '';
+        $person->numero_documento = $data['numero_documento'] ?? '';
+        $person->fecha_nacimiento = $data['fecha_nacimiento'] ?? '';
+        $person->edad = $data['edad'] ?? '';
+        $person->sexo = $data['sexo'] ?? '';
+        $person->identidad_genero = $data['identidad_genero'] ?? '';
+        $person->telefono_familiar = $data['telefono_familiar'] ?? '';
+        $person->nivel_escolaridad = $data['nivel_escolaridad'] ?? '';
+        $person->aporta_ingresos = $data['aporta_ingresos'] ?? '';
+        $person->tipo_afiliacion = $data['tipo_afiliacion'] ?? '';
+        $person->grupo_atencion_familiar = $data['grupo_atencion_familiar'] ?? '';
+        $person->habla_creole = $data['habla_creole'] ?? '';
+        $person->vacunas_covid = $data['vacunas_covid'] ?? '';
+        $person->dosis_vacuna = $data['dosis_vacuna'] ?? '';
+        $person->consumo_sustancias = $data['consumo_sustancias'] ?? '';
+        $person->tipo_sustancias = $data['tipo_sustancias'] ?? '';
+        $person->persona_recibe_visita = $data['persona_recibe_visita'] ?? '';
+        $person->cursos_vida_integrantes = $data['cursos_vida_integrantes'] ?? '';
+        $person->ocupacion_integrantes = $data['ocupacion_integrantes'] ?? '';
+        $person->situacion_discapacidad = $data['situacion_discapacidad'] ?? '';
+        $person->tipo_discapacidad = $data['tipo_discapacidad'] ?? '';
+        $person->atencion_integral = $data['atencion_integral'] ?? '';
+        $person->vinculacion_sgsss = $data['vinculacion_sgsss'] ?? '';
+        $person->percepcion_funcionalidad = $data['percepcion_funcionalidad'] ?? '';
+        $person->cuidador_principal = $data['cuidador_principal: false,'] ?? '';
+        $person->escala_zarit = $data['escala_zarit'] ?? '';
+        $person->userid = $data['userid'] ?? '';
+        $person->viviendaid = $data['viviendaid'] ?? '';
+        
+
+        
         // Guardamos el objeto en la base de datos
-        $childhood->save();
+        $person->save();
+    
+    
         // Retornamos una respuesta de éxito
-        return response()->json(['message' => 'Formulario Registrado Correctamente']);
+        return response()->json(['message' => 'Datos insertados correctamente']);
+
+
+
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Request $request, childhood $childhood)
+    public function show(Request $request, person $person)
     {
-        $data = $request->json()->all();
+        //$data = $request->json()->all();
+        $data = $request->all(); 
+
         //var_dump($data);exit();
         $userid = $data['userid'];
-        // $fecha1 = $data['fecha1'];
-        // $fecha2 = $data['fecha2'];
+        //$fecha1 = $data['fecha1'];
+        //$fecha2 = $data['fecha2'];
+        //$viviendaid = $data['viviendaid'];
         
 
-        $childhood = childhood::where('userid', $userid)
+        $person = person::where('userid', $userid)
                          ->where(function($query) use ($data) {
                             if (isset($data['id'])) {
-                                $query->orWhere('id', $data['id']);
+                                $query->Where('id', $data['id']);
                             }
-                            if (isset($data['personaid'])) {
-                                $query->orWhere('personaid', $data['personaid']);
+                            if (isset($data['userid'])) {
+                                $query->Where('userid', $data['userid']);
+                            }
+                            if (isset($data['viviendaid'])) {
+                                $query->Where('viviendaid', $data['viviendaid']);
                             }
                             //$query->whereBetween(\DB::raw('DATE(created_at)'), [$fecha1, $fecha2]);
                          })
                          ->get();
-        $dataArray = array($childhood);                 
-        return $dataArray;
 
-    }  
+       
+
+        $dataArray = array($person);                 
+        return $dataArray;
+    }
 
     /**
      * Update the specified resource in storage.
@@ -133,8 +157,43 @@ class childhoodController extends Controller
         $id = $data['id'];
         
         $rol_familiar = $data['rol_familiar'];
-        $tabla = childhood::where('id', $id)
-                   
+        $primer_nombre = $data['primer_nombre'];
+        $segundo_nombre = $data['segundo_nombre'];
+        $primer_apellido = $data['primer_apellido'];
+        $segundo_apellido = $data['segundo_apellido'];
+        $tipo_documento = $data['tipo_documento'];
+        $numero_documento = $data['numero_documento'];
+        $fecha_nacimiento = $data['fecha_nacimiento'];
+        $edad = $data['edad'];
+        $sexo = $data['sexo'];
+        $identidad_genero = $data['identidad_genero'];
+        $telefono_familiar = $data['telefono_familiar'];
+        $nivel_escolaridad = $data['nivel_escolaridad'];
+        $aporta_ingresos = $data['aporta_ingresos'];
+        $tipo_afiliacion = $data['tipo_afiliacion'];
+        $grupo_atencion_familiar = $data['grupo_atencion_familiar'];
+        $habla_creole = $data['habla_creole'];
+        $vacunas_covid = $data['vacunas_covid'];
+        $dosis_vacuna = $data['dosis_vacuna'];
+        $consumo_sustancias = $data['consumo_sustancias'];
+        $tipo_sustancias = $data['tipo_sustancias'];
+        $persona_recibe_visita = $data['persona_recibe_visita'];
+        $cursos_vida_integrantes = $data['cursos_vida_integrantes'];
+        $ocupacion_integrantes = $data['ocupacion_integrantes'];
+        $situacion_discapacidad = $data['situacion_discapacidad'];
+        $tipo_discapacidad = $data['tipo_discapacidad'];
+        $atencion_integral = $data['atencion_integral'];
+        $vinculacion_sgsss = $data['vinculacion_sgsss'];
+        $percepcion_funcionalidad = $data['percepcion_funcionalidad'];
+        $cuidador_principal = $data['cuidador_principal: false,'];
+        $escala_zarit = $data['escala_zarit'];
+        $userid = $data['userid'];
+        $personid = $data['personid'];
+
+
+        $tabla = person::where('id', $id)
+                   ->where('userid', $userid)
+                   ->where('personid', $personid)
                    ->firstOrFail();
 
         $tabla->rol_familiar = $rol_familiar;
@@ -152,7 +211,12 @@ class childhoodController extends Controller
         $data = $request->json()->all();
         //var_dump($data);exit();
         $id = $data['id'];
-        childhood::where('id', $id)->where('id', $id)->delete();
+        $userid = $data['userid'];
+        $personid = $data['personid'];
+        person::where('id', $id)->where('id', $id)
+                                ->where('userid', $userid)
+                                ->where('personid', $personid)
+                                ->delete();
     
         return response()->json(['message' => 'Dato borrado correctamente']);
 
