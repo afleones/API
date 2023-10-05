ab4f745f77905fd80bd2236b80581f013112794b
afleones <afleones@misena.edu.co>
2023-10-05 16:36:21 -0500

    array

--- app/Http/Controllers/personController.php
+++ app/Http/Controllers/personController.php
@@ -10,91 +10,91 @@ class personController extends Controller
     /**
      * Display a listing of the resource.
      */
-public function index()
-{
-    $people = DB::table('person')->select(
-        'id',
-        'rol_familiar',
-        'primer_nombre',
-        'segundo_nombre',
-        'primer_apellido',
-        'segundo_apellido',
-        'tipo_documento',
-        'numero_documento',
-        'fecha_nacimiento',
-        'edad',
-        'sexo',
-        'identidad_genero',
-        'telefono_familiar',
-        'nivel_escolaridad',
-        'aporta_ingresos',
-        'tipo_afiliacion',
-        'grupo_atencion_familiar',
-        'habla_creole',
-        'vacunas_covid',
-        'dosis_vacuna',
-        'consumo_sustancias',
-        'tipo_sustancias',
-        'persona_recibe_visita',
-        'cursos_vida_integrantes',
-        'ocupacion_integrantes',
-        'situacion_discapacidad',
-        'tipo_discapacidad',
-        'atencion_integral',
-        'vinculacion_sgsss',
-        'percepcion_funcionalidad',
-        'cuidador_principal',
-        'escala_zarit',
-        'userid',
-        'viviendaid'
-    )->get()->toArray();
-
-    $formattedPeople = [];
-
-    foreach ($people as $person) {
-        $formattedPerson = [
-            'id' => $person->id,
-            'rolFamiliar' => $person->rol_familiar,
-            'primerNombre' => $person->primer_nombre,
-            'segundoNombre' => $person->segundo_nombre,
-            'primerApellido' => $person->primer_apellido,
-            'segundoApellido' => $person->segundo_apellido,
-            'tipoDocumento' => $person->tipo_documento,
-            'numeroDocumento' => $person->numero_documento,
-            'fechaNacimiento' => $person->fecha_nacimiento,
-            'edad' => $person->edad,
-            'sexo' => $person->sexo,
-            'identidadGenero' => $person->identidad_genero,
-            'telefonoFamiliar' => $person->telefono_familiar,
-            'nivelEscolaridad' => $person->nivel_escolaridad,
-            'aportaIngresos' => $person->aporta_ingresos,
-            'tipoAfiliacion' => $person->tipo_afiliacion,
-            'grupoAtencionFamiliar' => $person->grupo_atencion_familiar,
-            'hablaCreole' => $person->habla_creole,
-            'vacunasCovid' => $person->vacunas_covid,
-            'dosisVacuna' => $person->dosis_vacuna,
-            'consumoSustancias' => $person->consumo_sustancias,
-            'tipoSustancias' => $person->tipo_sustancias,
-            'personaRecibeVisita' => $person->persona_recibe_visita,
-            'cursosVidaIntegrantes' => $person->cursos_vida_integrantes,
-            'ocupacionIntegrantes' => $person->ocupacion_integrantes,
-            'situacionDiscapacidad' => $person->situacion_discapacidad,
-            'tipoDiscapacidad' => $person->tipo_discapacidad,
-            'atencionIntegral' => $person->atencion_integral,
-            'vinculacionSgsss' => $person->vinculacion_sgsss,
-            'percepcionFuncionalidad' => $person->percepcion_funcionalidad,
-            'cuidadorPrincipal' => $person->cuidador_principal,
-            'escalaZarit' => $person->escala_zarit,
-            'userId' => $person->userid,
-            'viviendaId' => $person->viviendaid,
-        ];
-
-        $formattedPeople[] = $formattedPerson;
+    public function index()
+    {
+        $people = DB::table('person')->select(
+            'id',
+            'rol_familiar',
+            'primer_nombre',
+            'segundo_nombre',
+            'primer_apellido',
+            'segundo_apellido',
+            'tipo_documento',
+            'numero_documento',
+            'fecha_nacimiento',
+            'edad',
+            'sexo',
+            'identidad_genero',
+            'telefono_familiar',
+            'nivel_escolaridad',
+            'aporta_ingresos',
+            'tipo_afiliacion',
+            'grupo_atencion_familiar',
+            'habla_creole',
+            'vacunas_covid',
+            'dosis_vacuna',
+            'consumo_sustancias',
+            'tipo_sustancias',
+            'persona_recibe_visita',
+            'cursos_vida_integrantes',
+            'ocupacion_integrantes',
+            'situacion_discapacidad',
+            'tipo_discapacidad',
+            'atencion_integral',
+            'vinculacion_sgsss',
+            'percepcion_funcionalidad',
+            'cuidador_principal',
+            'escala_zarit',
+            'userid',
+            'viviendaid'
+        )->get();
+    
+        $formattedPeople = [];
+    
+        foreach ($people as $person) {
+            $formattedPerson = (object)[
+                'id' => $person->id,
+                'rolFamiliar' => $person->rol_familiar,
+                'primerNombre' => $person->primer_nombre,
+                'segundoNombre' => $person->segundo_nombre,
+                'primerApellido' => $person->primer_apellido,
+                'segundoApellido' => $person->segundo_apellido,
+                'tipoDocumento' => $person->tipo_documento,
+                'numeroDocumento' => $person->numero_documento,
+                'fechaNacimiento' => $person->fecha_nacimiento,
+                'edad' => $person->edad,
+                'sexo' => $person->sexo,
+                'identidadGenero' => $person->identidad_genero,
+                'telefonoFamiliar' => $person->telefono_familiar,
+                'nivelEscolaridad' => $person->nivel_escolaridad,
+                'aportaIngresos' => $person->aporta_ingresos,
+                'tipoAfiliacion' => $person->tipo_afiliacion,
+                'grupoAtencionFamiliar' => $person->grupo_atencion_familiar,
+                'hablaCreole' => $person->habla_creole,
+                'vacunasCovid' => $person->vacunas_covid,
+                'dosisVacuna' => $person->dosis_vacuna,
+                'consumoSustancias' => $person->consumo_sustancias,
+                'tipoSustancias' => $person->tipo_sustancias,
+                'personaRecibeVisita' => $person->persona_recibe_visita,
+                'cursosVidaIntegrantes' => $person->cursos_vida_integrantes,
+                'ocupacionIntegrantes' => $person->ocupacion_integrantes,
+                'situacionDiscapacidad' => $person->situacion_discapacidad,
+                'tipoDiscapacidad' => $person->tipo_discapacidad,
+                'atencionIntegral' => $person->atencion_integral,
+                'vinculacionSgsss' => $person->vinculacion_sgsss,
+                'percepcionFuncionalidad' => $person->percepcion_funcionalidad,
+                'cuidadorPrincipal' => $person->cuidador_principal,
+                'escalaZarit' => $person->escala_zarit,
+                'userId' => $person->userid,
+                'viviendaId' => $person->viviendaid,
+            ];
+    
+            $formattedPeople[] = $formattedPerson;
+        }
+    
+        return $formattedPeople;
     }
-
-    return $formattedPeople;
-}
-
+    
     /**
      * Store a newly created resource in storage.
      */
