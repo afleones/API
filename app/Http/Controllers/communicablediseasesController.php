a0a98662487b2b179e7d148c5e5d4ae1e5171238
afleones <afleones@misena.edu.co>
2023-10-07 10:36:16 -0500

    show nmethods endPoints

--- app/Http/Controllers/communicablediseasesController.php
+++ app/Http/Controllers/communicablediseasesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\communicablediseases;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class communicablediseasesController extends Controller
 {
