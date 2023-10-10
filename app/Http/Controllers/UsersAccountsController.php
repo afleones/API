c36f4f540d450831aa13438386c30ed9745e5932
afleones <afleones@misena.edu.co>
2023-10-10 16:16:08 -0500

    endPoint update users

--- app/Http/Controllers/UsersAccountsController.php
+++ app/Http/Controllers/UsersAccountsController.php
@@ -6,6 +6,7 @@ use App\Models\users_accounts;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\HasApiTokens;
 
 class UsersAccountsController extends Controller
 {
