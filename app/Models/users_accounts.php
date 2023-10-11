2e45a65998dd6106e85ac72c6cd19af249e7f770
afleones <afleones@misena.edu.co>
2023-10-11 09:35:29 -0500

    endPoint update users

--- app/Models/users_accounts.php
+++ app/Models/users_accounts.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class users_accounts extends Model
 {
