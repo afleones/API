a07ad556beeb1318a61dd1bdefd1aa5d064d47ac
Andres Leones Palacio <contacto@andresleones.com.co>
2023-11-03 10:16:20 -0500

    Update CorsMiddleware.php

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -16,13 +16,12 @@ class CorsMiddleware
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        //if (!$response instanceof \Symfony\Component\HttpFoundation\BinaryFileResponse) {
+
         $response->header('Access-Control-Allow-Origin', '*');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->header('Access-Control-Allow-Headers' , 'Authorization, Content-Type');
         $response->header('Access-Control-Expose-Headers' , 'Authorization');
         $response->header('Access-Control-Max-Age' , '86400');
-        //}
 
         return $response;
     }
