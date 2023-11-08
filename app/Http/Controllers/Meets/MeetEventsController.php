beeeffde0f58e0c3ebaa84a86930c675642a5f24
afleones <afleones@misena.edu.co>
2023-11-08 16:11:51 -0500

    update

--- app/Http/Controllers/Meets/MeetEventsController.php
+++ app/Http/Controllers/Meets/MeetEventsController.php
@@ -171,7 +171,7 @@ class MeetEventsController extends Controller
                     $guestEvent->save();
     
                     // Insertar la relación en la tabla intermedia 'eventsMeetsGuests'
-                    DB::table('eventsMeetsGuests')->insert([
+                    DB::table('meet000003eventsMeetsGuests')->insert([
                         'eventId' => $id,
                         'meetGuestsEventsId' => $guestEvent->id,
                     ]);
